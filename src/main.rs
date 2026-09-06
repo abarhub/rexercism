d82074855457e8e413a65b5a545cd0f29cf027c6
abarhub <abarhub@gmail.com>
2026-09-06 11:19:58 +0200

    reverse

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,57 @@
+use std::ops::Add;
+use unicode_segmentation::UnicodeSegmentation;
+
 fn hello_world() -> String {
-    return "Hello World".to_string();
+    "Hello World".to_string()
+}
+
+pub fn reverse(input: &str) -> String {
+    let mut res: String = "".to_string();
+    for c in input.graphemes(true).rev() {
+        res = res.add(c);
+    }
+    res
 }
 
 fn main() {
-    println!("{}", hello_world());
+    // hello
+    //println!("{}", hello_world());
+    println!("{}", reverse("Hello World"));
+}
+
+#[test]
+//#[ignore]
+fn test_hello_world() {
+    let output = hello_world();
+    let expected = "Hello World";
+    assert_eq!(output, expected);
+}
+
+#[test]
+//#[ignore]
+fn a_word() {
+    let input = "robot";
+    let output = reverse(input);
+    let expected = "tobor";
+    assert_eq!(output, expected);
+}
+
+#[test]
+//#[ignore]
+//#[cfg(feature = "grapheme")]
+fn grapheme_cluster_with_pre_combined_form() {
+    let input = "Würstchenstand";
+    let output = reverse(input);
+    let expected = "dnatsnehctsrüW";
+    assert_eq!(output, expected);
+}
+
+#[test]
+//#[ignore]
+//#[cfg(feature = "grapheme")]
+fn grapheme_clusters() {
+    let input = "ผู้เขียนโปรแกรม";
+    let output = reverse(input);
+    let expected = "มรกแรปโนยขีเผู้";
+    assert_eq!(output, expected);
 }
