9ba6c1909f3a6de548b1b09fb5482dfe2363f308
abarhub <abarhub@gmail.com>
2026-09-06 17:01:32 +0200

    ajout echo

--- src/shell.rs
+++ src/shell.rs
@@ -1,4 +1,4 @@
-use std::io::BufRead;
+use std::io::{stdout, BufRead, Write};
 use std::{fs, io};
 
 pub fn run_shell() {
@@ -6,22 +6,18 @@ pub fn run_shell() {
     let mut line = "".to_string();
     loop {
         line = "".to_string();
-        println!(">");
+        print!("> ");
+        stdout().flush().expect("Failed to flush stdout");
         let stdin = io::stdin();
         let s = stdin.lock().read_line(&mut line);
         if let Ok(_) = s {
-            //for line in stdin.lock().lines() {
-            /*if line.ends_with('\n') {
-                line = line[0..line.len() - 1].to_string()
-            }*/
             let len = line.trim_end_matches(&['\r', '\n'][..]).len();
             line.truncate(len);
-            println!("!{}!", line.clone());
+            //println!("!{}!", line.clone());
             let fin = traite(line.clone());
             if fin {
                 break;
             }
-            //}
         }
     }
 }
@@ -33,6 +29,9 @@ fn traite(ligne: String) -> bool {
     } else if ligne2 == ("ls") || ligne2.starts_with("ls ") {
         let ligne3 = ligne2.trim_start_matches("ls");
         command_ls(ligne3.to_string()).expect("TODO: panic message");
+    } else if ligne2 == ("echo") || ligne2.starts_with("echo ") {
+        let ligne3 = ligne2.trim_start_matches("echo");
+        command_echo(ligne3.to_string());
     }
     false
 }
@@ -54,4 +53,9 @@ fn command_ls(mut ligne: String) -> Result<(), Box<dyn std::error::Error>> {
     }
 
     Ok(())
+}
+
+fn command_echo(mut ligne: String) {
+    ligne = ligne.trim().to_string();
+    println!("{}", ligne);
 }
